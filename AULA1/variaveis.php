8bcaf6b0b6a48f1213e1ba264012ea049e8b446c
MuriloColetta <mucoltta11@gmail.com>
2025-08-01 16:50:48 -0300

    01.08.25

---
+++ AULA1/variaveis.php
@@ -0,0 +1,9 @@
+<?php
+    echo"Olá \n";
+    $nome = "Murilo \n";
+    $idade = "16";
+    $ano_atual = "2025";
+
+    $data_nasc = $ano_atual - $idade;
+    echo $nome, "você nasceu em: ", $data_nasc;
+?>

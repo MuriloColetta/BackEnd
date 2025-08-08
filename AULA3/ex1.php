5c4db5373b00b279cacf1917472be60f4f375331
Murilo Della Coletta <mucoletta11@gmail.com>
2025-08-08 16:57:59 -0300

    Delete AULA3 directory

--- AULA3/ex1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$idade = readline("Digite sua idade: ");
-
-if ($idade >= 18) {
-    echo "Você é maior de idade.";
-} else {
-    echo "Você é menor de idade.";
-}
-?>
--- AULA3/ex6.php
+++
@@ -1,7 +0,0 @@
-<?php
-$n = readline("Digite um número: ");
-
-for ($i = $n; $i >= 1; $i--) {
-    echo "Número: $i \n";
-}
-?>

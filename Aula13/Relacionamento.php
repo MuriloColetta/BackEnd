277e2e7fe0a0e505d3669861509f0d876bb760b1
Murilo Della Coletta <mucoletta11@gmail.com>
2025-10-10 16:59:26 -0300

    10/10

---
+++ Aula13/Relacionamento.php
@@ -0,0 +1,12 @@
+Relacionamento entre classes: a referência para análise será sempre os objetos daquela determinada classe
+
+Associação: conexão entre duas classes, onde os objetos podem interagir compartilhando informações.
+    Exemplo: Pessoa e Empresa. Pode-se escolher um objeto de cada classe para análise -> Joãozinho trabalha na empresa Ajnomoto. Nesse tipo de relacionaento as classes podem existir
+    independente uma da outra
+
+Agregação: conexão entre duas classe, onde uma delasé uma parte de outra, mas o objeto "parte" pode existir independente do objeto "todo".
+    Exemplo: Carro e Motorista -> Hayron pode dirigir um Del Rey, sendo uma "parte" que compõe o carro como um todo, mas pode existir independente do carro
+
+Composição: é conexão mais forte entre classes, onde a existência do objeto "parte" é diretamete ligada ao objeto "todo". Isso quer dizer que se o todo deixa de existir, a "parte" é
+também deixa.
+    Exemplo: Carro e Motor. Relacionando os objetos -> O carro Audi R8 pode conter um motor V12 %.@ aspirado, mas o motor deixa de existir se o carro 
